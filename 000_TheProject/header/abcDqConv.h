93cf81279041528da2c82f570c6d5848a45e39ed
Aleksander Wyżykowski <wyzykjr@gmail.com>
2023-11-02 13:33:53 +0100

    untested

--- 000_TheProject/header/abcDqConv.h
+++ 000_TheProject/header/abcDqConv.h
@@ -8,6 +8,9 @@
 #ifndef HEADER_ABCDQCONV_H_
 #define HEADER_ABCDQCONV_H_
 
+#include <math.h>
 #include "abcDqConvStructs.h"
+
+void abcToDq(struct ABCDQ_STRUCT *data, float a, float b, float c, float eAngle);
 
 #endif /* HEADER_ABCDQCONV_H_ */
--- 000_TheProject/source/abcDqConv.c
+++ 000_TheProject/source/abcDqConv.c
@@ -7,4 +7,36 @@
 
 #include "abcDqConv.h"
 
+void abcdqStructInit(struct ABCDQ_STRUCT *data){
+    data->a = 0;
+    data->b = 0;
+    data->c = 0;
 
+    data->d = 0;
+    data->q = 0;
+    data->eAngle = 0;
+    data->zero = 0;
+
+}
+
+void abcToDq(struct ABCDQ_STRUCT *data, float a, float b, float c, float eAngle){
+    data->a = a;
+    data->b = b;
+    data->c = c;
+    data->eAngle = eAngle;
+
+    data->d = 2.0/3.0 * ((data->a*sin(eAngle))+(data->b*sin((data->eAngle-(2.0*M_PI/3.0)))+(data->c*sin((eAngle+(2.0*M_PI/3.0))))));
+    data->q = 2.0/3.0 * ((data->a*cos(eAngle))+(data->b*cos((data->eAngle-(2.0*M_PI/3.0)))+(data->c*cos((eAngle+(2.0*M_PI/3.0))))));
+    data->zero = (a+b+c)/3.0;
+}
+
+void dqToAbc(struct ABCDQ_STRUCT *data, float d, float q, float eAngle){
+    //eAngle is electrical angle IN RADIANS
+    data->d = d;
+    data->q = q;
+    data->eAngle = eAngle;
+
+    data->a = data->d*sin(eAngle) + data->q*cos(eAngle) + data->zero;
+    data->b = data->d*sin((data->eAngle-((2.0*M_PI)/3.0))) + data->q*cos((data->eAngle-((2.0*M_PI)/3.0))) + data->zero;
+    data->c = data->d*sin((data->eAngle+((2.0*M_PI)/3.0))) + data->q*cos((data->eAngle+((2.0*M_PI)/3.0))) + data->zero;
+}
